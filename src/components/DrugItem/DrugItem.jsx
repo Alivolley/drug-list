33d1c8c4c8514d7e2739ad9d4ab7b41f87d4ef6b
Ali <alicryptovolley@gmail.com>
2023-01-28 14:40:29 +0330

    home apis finished

--- src/components/DrugItem/DrugItem.jsx
+++ src/components/DrugItem/DrugItem.jsx
@@ -2,25 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./DrugItem.css";
 
-const DrugItem = () => {
+const DrugItem = ({ faName, enName, cure, drugId }) => {
    return (
       <div className="drugItem">
          <div className="drugItem-right">
             <div className="drugItem-filed">
                <label className="drugItem-filed__label">نام عملی :</label>
-               <p className="drugItem-filed__answer">lorem saz</p>
+               <p className="drugItem-filed__answer">{enName}</p>
             </div>
             <div className="drugItem-filed">
                <label className="drugItem-filed__label">نام دارو :</label>
-               <p className="drugItem-filed__answer">استامین</p>
+               <p className="drugItem-filed__answer">{faName}</p>
             </div>
             <div className="drugItem-filed">
                <label className="drugItem-filed__label">درمان :</label>
-               <p className="drugItem-filed__answer">سردرد</p>
+               <p className="drugItem-filed__answer">{cure}</p>
             </div>
          </div>
          <div className="drugItem-left">
-            <Link to="/drug/1" className="drugItem-btn">
+            <Link to={`/drug/${drugId}`} className="drugItem-btn">
                جزئیات بیشتر
             </Link>
          </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DrugItem from "../../components/DrugItem/DrugItem";
 import "./Home.css";
 
 const Home = () => {
+   const [drugsList, setDrugsList] = useState();
+
+   useEffect(() => {
+      fetch("http://localhost:4000/drugs")
+         .then((res) => res.json())
+         .then((data) => setDrugsList(data))
+         .catch((err) => console.log(err));
+   }, []);
+
+   console.log(drugsList);
+
    return (
       <div className="home">
-         <DrugItem />
-         <DrugItem />
-         <DrugItem />
-         <DrugItem />
-         <DrugItem />
-         <DrugItem />
+         {drugsList ? (
+            drugsList.map((drug) => <DrugItem key={drug.id} faName={drug.fa_name} enName={drug.en_name} cure={drug.cure} drugId={drug.id} />)
+         ) : (
+            <p className="loading">Loading ...</p>
+         )}
       </div>
    );
 };
